17084ed653374b43c32ee8301aa468725367b2ea
sethgalyon <github@sethgalyon.slmail.me>
2022-06-17 23:44:49 -0400

    Make board generation truly random

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # sudoku
 Sudoku in the terminal window written in Python
-
-# to do
-- Make randomization of board truly random instead of psuedo-random
--- sudoku.py
+++ sudoku.py
@@ -1,81 +1,66 @@
-import random
+from random import shuffle
 import re
 
-def shift(x,n):
-    return x[n:] + x[:n]
 
-def randomize_screen(difficulty, working_board, solved_board, writeable_cells):
-    for p in range(len(working_board)):
-        for o in range(len(working_board)):
-            working_board[o][p] = ' '
-            writeable_cells[o][p] = ' '
-    
-    b = [1,2,3,4,5,6,7,8,9]
-    random.shuffle(b)
-    for y in range(len(working_board)):
-        working_board[0][y] = b[y]
-        
-    p = 1
-    c = shift(b,3)
-    while True:
-        for y in range(len(working_board)):
-            working_board[p][y] = c[y]
-        p = p + 1
-        if p == 9:
+def valid_num(board, row, col, num):
+    if num in board[row]:
+        return False
+
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+    for i in range(box_row, (box_row + 3)): 
+        for j in range(box_col, (box_col + 3)): 
+            if board[i][j] == num: 
+                return False
+
+    return True
+
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i,j)
+
+    return
+
+
+def generate_board(board):
+    nums = [1,2,3,4,5,6,7,8,9]
+    for i in range(0, 81):
+        row = i // 9
+        col = i % 9
+        if board[row][col] == 0:
+            shuffle(nums)
+            for num in nums:
+                if valid_num(board, row, col, num):
+                    board[row][col] = num
+                    if not find_empty_cell(board):
+                        return True
+                    elif generate_board(board):
+                        return True
             break
-        elif p == 3 or p == 6:
-            c = shift(c,1)
-        else:
-            c = shift(c,3)
-    
-    if random.getrandbits(1) == 1:
-        for y in range(len(working_board)):
-            c[y] = working_board[y][0]
-                
-        p = 1
-        c = shift(c,3)
-        while True:
-            for y in range(len(working_board)):
-                working_board[y][p] = c[y]
-            p = p + 1
-            if p == 9:
-                break
-            elif p == 3 or p == 6:
-                c = shift(c,1)
-            else:
-                c = shift(c,3)
-    else:
-        for y in range(len(working_board)):
-            c[y] = working_board[0][y]
-            
-        p = 1
-        c = shift(c,3)
-        while True:
-            for y in range(len(working_board)):
-                working_board[p][y] = c[y]
-            p = p + 1
-            if p == 9:
-                break
-            elif p == 3 or p == 6:
-                c = shift(c,1)
-            else:
-                c = shift(c,3)
-    
-    for solved_boardcol in range(9):
-        for solved_boardrow in range(9):
-            solved_board[solved_boardrow][solved_boardcol] = \
-                working_board[solved_boardrow][solved_boardcol]
-    
-    p = 0
-    while True:
-        if p == 9:
-            break
-        random.shuffle(b)
+
+    board[row][col] = 0
+    return False
+
+
+def remove_cells(board, writeable_cells, difficulty):
+    row = 0
+    nums = [0,1,2,3,4,5,6,7,8]
+    while row < 9:
+        shuffle(nums)
         for i in range(difficulty):
-            index = b[i] - 1
-            working_board[p][index] = ' '
-            writeable_cells[p][index] = 1
-        p = p + 1
+            col = nums[i]
+            board[row][col] = ' '
+            writeable_cells[row][col] = 1
+
+        row += 1
+
 
 def print_screen(board):
     print('\n    A   B   C   D   E   F   G   H   I\n')
@@ -85,47 +70,46 @@ def print_screen(board):
                       f'{row[6]}   {row[7]}   {row[8]}')
         if i == 2 or i == 5:
             print('   --- --- --- --- --- --- --- --- ---')
+
     print()
+
 
 def parse_input(input):
     cell = ()
     num = None
-    message = 'Invalid input. Type the column, row, and a space followed by '\
-              'the number desired. (ex. A2 9) \n'
 
     valid_input = re.match(r'([A-I])([1-9])\s+([1-9])\s*$', input.upper())
 
     if valid_input:
         cell = (int(valid_input[2]) - 1, ord(valid_input[1]) - 65)
         num = int(valid_input[3])
-        message = ''
 
-    return {'cell': cell, 'number': num, 'message': message}
+    return {'cell': cell, 'num': num}
+
 
 def confirm_board(board):
     rows = {i: [] for i in range(len(board))}
     columns = {i: [] for i in range(len(board))}
     boxes = {i: [] for i in range(len(board))}
-    
     for row in range(len(board)):
-        for column in range(len(board)):
-            if board[row][column] == '.':
+        for col in range(len(board)):
+            if board[row][col] == '.':
                 continue
-            elif board[row][column] in rows[row] or \
-                board[row][column] in columns[column] or \
-                board[row][column] in boxes[row // 3 + (column // 3) * 3]:
+            elif board[row][col] in rows[row] or \
+                board[row][col] in columns[col] or \
+                board[row][col] in boxes[row // 3 + (col // 3) * 3]:
                 return False
             else:
-                rows[row].append(board[row][column])
-                columns[column].append(board[row][column])
-                boxes[row // 3 + (column // 3) * 3].append(board[row][column])
-    
+                rows[row].append(board[row][col])
+                columns[col].append(board[row][col])
+                boxes[row // 3 + (col // 3) * 3].append(board[row][col])
+
     return True
 
+
 def play_sudoku():
-    working_board   = [[' ' for _ in range(9)] for _ in range(9)]
-    solved_board    = [[' ' for _ in range(9)] for _ in range(9)]
-    writeable_cells = [[' ' for _ in range(9)] for _ in range(9)]
+    board = [[0 for _ in range(9)] for _ in range(9)]
+    writeable_cells = [[0 for _ in range(9)] for _ in range(9)]
 
     difficulty = input('\nPlease choose a difficulty (easy, medium, hard):')
     if difficulty == 'easy' or difficulty == 'e':
@@ -135,16 +119,18 @@ def play_sudoku():
     elif difficulty == 'hard' or difficulty == 'h':
         difficulty = 7
     elif difficulty == 'quit' or difficulty == 'q':
-        print('\nThanks for Playing!')
+        print('\nThanks for Playing!\n')
         return
     else:
-        print("\nInvalid difficulty. Please re-enter difficulty (type 'quit' to quit).")
+        print("\nInvalid difficulty. Please re-enter difficulty or type 'quit' to quit.")
         play_sudoku()
         return
 
-    randomize_screen(difficulty, working_board, solved_board, writeable_cells)
-    
-    print_screen(working_board)
+    generate_board(board)
+
+    remove_cells(board, writeable_cells, difficulty)
+
+    print_screen(board)
 
     helpmessage = "Type 'done'  to calculate board.\nType 'reset' to reset.\nType "\
                   "'quit'  to quit the game.\nType 'help'  to show this message again.\n"
@@ -154,51 +140,44 @@ def play_sudoku():
     while True:
         prompt = input('Input cell index and number desired (ex. A2 9):')
         if prompt == 'quit' or prompt == 'q':
-            print('\nThanks for playing!')
+            print('\nThanks for playing!\n')
             break
         elif prompt == 'help' or prompt == 'h':
-            print_screen(working_board)
+            print_screen(board)
             print(helpmessage)
-            continue
-        elif prompt == 'backdoor':
-            print_screen(solved_board)
-            print('Solution displayed.')
-            print_screen(working_board)
             continue
         elif prompt == 'reset' or prompt == 'r':
             play_sudoku()
             break
         elif prompt == 'done' or prompt == 'd':
-           if not confirm_board(working_board):
+           if not confirm_board(board):
                 print('\nPuzzle Not Correct! Try Again.')
-                print_screen(working_board)
+                print_screen(board)
                 continue
            else:
-               print('\nCongratulations! The Puzzle is Correct!\n')
-               again = input("Play Again? ('yes' to play again):")
-               if again == 'yes' or again == 'y':
-                   play_sudoku()
-                   break
-               print('\nThanks for Playing!')
-               break
+                print('\nCongratulations! The Puzzle is Correct!\n')
+                prompt = input("Play Again? ('yes' to play again):")
+                if prompt == 'yes' or prompt == 'y':
+                    play_sudoku()
+                    break
+                print('\nThanks for Playing!\n')
+                break
         
         result = parse_input(prompt)
         cell = result['cell']
-        number = result['number']
-        message = result['message']
-        if cell and number:
-            rowno, colno = cell
-            if writeable_cells[rowno][colno] != 1:
+        num = result['num']
+        if cell and num:
+            row, col = cell
+            if writeable_cells[row][col] != 1:
                 print("\nCan't change initial number. Try Again.")
-                print_screen(working_board)
-                continue
-            
-            working_board[rowno][colno] = number
-        
-        print_screen(working_board)
-        
-        if message != '':
-            print(message)
+            else:
+                board[row][col] = num
+            print_screen(board)
+        else:
+            print_screen(board)
+            print('Invalid input. Type the column, row, and a space followed by '\
+                  'the number desired. (ex. A2 9) \n')
+
 
 if __name__ == '__main__':
     play_sudoku()
